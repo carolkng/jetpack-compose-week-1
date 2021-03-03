28be43ed22f4dd32c5ef7dbe62fb5578c2241a02
Carol Ng <carol@carolkng.com>
2021-03-02 23:17:39 -0800

    ktlint

--- app/src/main/java/com/example/androiddevchallenge/ui/home/Home.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/home/Home.kt
@@ -1,9 +1,25 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge.ui.home
 
 import android.content.Context
 import android.content.Intent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Button
 import androidx.compose.material.Text
@@ -26,12 +42,14 @@ fun Home(context: Context) {
             listOf("dog1", "dog2").forEach {
                 val dog = Dogabase.getDog(it)
 
-                Button(onClick = {
-                    Intent("DetailActivity").apply {
-                        putExtra("dogId", dog.id)
-                        context.startActivity(this)
+                Button(
+                    onClick = {
+                        Intent("DetailActivity").apply {
+                            putExtra("dogId", dog.id)
+                            context.startActivity(this)
+                        }
                     }
-                }) {
+                ) {
                     DogCard(dog)
                 }
             }
